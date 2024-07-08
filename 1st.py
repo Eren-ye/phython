8bd102791bcb2fef5775b335263f572cb382b9e0
lordd6171@gmail.com <gyomei987@outlook.com>
2024-07-08 16:36:33 +0545

    update

---
+++ 1st.py
@@ -0,0 +1,72 @@
+# x=5
+# y=7
+# z=8
+# a=10
+# if x>y>z>a:
+#     print(" x is greater")
+# elif(y>z>x>a):
+#     print(" y is greater")
+# elif(z>a>y>x):
+#     print(" z is greater")
+# else:
+#     print(" a is greater")
+
+
+# abc= min(x,y,z,a) 
+# print(abc)
+# print(" Enter the marks:")
+
+a=input("Enter the marks of 1st subject: ")
+b=input("Enter the marks of 2st subject: ")
+c=input("Enter the marks of 3st subject: ")
+d=input("Enter the marks of 4st subject: ")
+e=input("Enter the marks of 5st subject: ")
+
+total= int(a)+int(b)+int(c)+int(d)+int(e)
+print(f"Total marks:{total} ")
+
+# principle=int(input(" Enter the principle:"))
+# time=int(input(" Enter the time:"))
+
+# rate=int(input("Enter the rate: "))
+# si=(principle*time*rate)/100
+# print(f"Simple interest:{si}")
+# pre= (total/5)*100
+# print("Total marks: ",total)
+# if pre>=60 and pre<=100:
+#     print("A")
+# elif pre>=45 and pre<60:
+#     print(" B")
+# elif pre>=35 and pre<45:
+#     print("C")  
+# else :
+#     print(" Fali")
+
+# age=int(input(" Enter your age"))
+# if age>=18 and age<=40:
+#     print(" You are eligible to vote")
+# else:
+#     print(" You are not eligible")
+
+# password=input(" Enter your password")
+# uername=input(" Ente your username")
+# if password=="ADMIN" and uername== "ADMIN":
+#     print(" SUCCESFULYY")
+# else:
+#     print(" incorrect passord")
+
+
+# a=60
+# b=30
+# d=50
+# if a>b:
+#     if b<d:
+#         print(" NEsted if")
+#     else:
+#         print(" Tha vaay na")
+
+ # else:
+# USERNAME=   input(" Enter the username :")
+# PASSWORD=   input(" Enter the password :")
+# Address= input(" Enter the address :")
+# print(f"User name is :{USERNAME} and password is {PASSWORD} and the address is {Address}")
---
+++ fi.py
@@ -0,0 +1,7 @@
+num= int(input("Enter a number to find the multiplication:"))
+i=1
+while i<=10:
+  mul = i * num
+  print(f"{num} * {i} = {mul}")
+  i += 1
+  
